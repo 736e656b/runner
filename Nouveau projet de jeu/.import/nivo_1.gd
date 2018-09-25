0ff36c0b9519ac3c359900b59064557767bb14ae
kimiko <elie-s@live.fr>
2018-09-25 21:00:09 +0200

    simplification de gravité
    simplification de gravité

--- Nouveau projet de jeu/.import/nivo_1.gd
+++ Nouveau projet de jeu/.import/nivo_1.gd
@@ -3,24 +3,19 @@ var speed = 500
 var direction = Vector2() 
 var width
 var height
-const GRAVITY = 20
-var acceleration = 1
+var GRAVITY = 40
+
 
 
 func _ready():
-	#position.y = 0
-	position = get_viewport_rect().size / 2 #place position au milieu de l'ecran
-	direction.x = 0 
-	direction.y = 0
-	direction = direction.normalized()
-	width = get_viewport_rect().size.x   
-	height = get_viewport_rect().size.y
+    width = get_viewport_rect().size.x   
+    height = get_viewport_rect().size.y
+    position.y = 0
+    position.x = width / 2 #place position au milieu de l'ecran
 
 func _process(delta):
-	pass
+    pass
+    
 func _physics_process(delta):
-	acceleration += 0.5
-	position.y += GRAVITY * delta * acceleration
-	
-	
-	pass
+    GRAVITY *= 1.05
+    position.y += GRAVITY * delta 
